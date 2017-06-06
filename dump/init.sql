9e62a3461e6070ac6cad813b4b1d18910f12d753
Israel Jardim <israjardim21@gmail.com>
2017-06-05 22:13:15 -0300

    Atualização do arquivo read me

--- dump/init.sql
+++ dump/init.sql
@@ -1,3 +1,5 @@
+CREATE DATABASE  IF NOT EXISTS `reserva_salas` /*!40100 DEFAULT CHARACTER SET utf8 */;
+USE `reserva_salas`;
 -- MySQL dump 10.13  Distrib 5.7.12, for Win64 (x86_64)
 --
 -- Host: localhost    Database: reserva_salas
@@ -158,4 +160,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2017-06-05 21:41:06
+-- Dump completed on 2017-06-05 21:43:42
